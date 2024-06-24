dcdd355f70cd6287b6b486e87024c8e815014a11
Julien Blacteau <julien-blacteau@aviss-securite.fr>
2024-06-24 15:58:25 +0200

    Affichage Attribut JSON

--- src/components/notes/NoteTechnique.js
+++ src/components/notes/NoteTechnique.js
@@ -4,11 +4,21 @@ import './NoteStyle.css';
 
 const Technique = () => {
 
-  console.log('Attributs')
+  console.log(Attributs)
   return (
     <div className="notestyle-container">
       <h3 className="notestyle-title">TECHNIQUE</h3>
-        
+        {
+            Attributs.map( attribut =>{
+
+              return(
+                <div className="notestyle-attribute" key={ attribut.id }>
+                  {attribut.name}
+                </div>   
+              )
+            
+            })
+          }
               
     </div>
   );
